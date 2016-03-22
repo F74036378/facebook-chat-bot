5cef6f1c446a39e362b09fe9aa27406cdbd27f2b
Larry <pudding850806@gmail.com>
2016-03-22 22:33:19 +0800

    refactor and clever

--- facebook-chat-bot.js
+++ facebook-chat-bot.js
@@ -3,10 +3,8 @@ var login = require("facebook-chat-api");
 var respond = require("./functions/respond");
 
 var larryID = 100001567752835;
-
 var goodTimes = 0;
 
-	//res["課表"] =				{ attachment: fs.createReadStream("./attachment/curriculum.png") }
 
 login({email: "puddingddoogg@gmail.com", password: "mz6s3zfe"}, function callback (err, api) {
 	if(err) return console.error(err);
@@ -16,41 +14,27 @@ login({email: "puddingddoogg@gmail.com", password: "mz6s3zfe"}, function callbac
 	var stop = api.listen(function(err, message){
 		if(err) return console.error(err);
 
-		if(message.body){
-			var str = "";
+		req = message.body.toLowerCase();
+		if(req){
 
-			if(message.body.charAt(0) == '/'){
+			var str = respond.getResponse(req);
+			api.sendMessage(str, message.threadID);
 
-				str = respond.getSysResponse(message.body);
-				if(!str) str = "no this command";
-
-				api.sendMessage(str, message.threadID);
-
-				if(message.body === '/stop'){
-					return stop();
-				}
-
-			} else if((str = respond.getResponse(message.body))){
-
-				api.sendMessage(str, message.threadID);
-
-			} else {
-
-				str = respond.getQuestion();
-				api.sendMessage(str, message.threadID);
-
+			if(req === '/stop'){
+				return stop();
 			}
+
 		}
 
 		
 		if(message["attachments"][0] != undefined && message["attachments"][0]["type"] === "sticker"){
 
 			str = "";
-			goodTimes++;
 			for(i=0 ; i<goodTimes%10+1 ; i++){
 				str += "讚個屁";
 			}
 			api.sendMessage(str, message.threadID);
+			goodTimes++;
 			
 		}
 		
--- functions/respond.js
+++ functions/respond.js
@@ -1,35 +1,26 @@
 console.log("init respond");
 
+amountOfQuestion = 0;
 questionList = "";
 questionList += "我聽不懂你在說什麼\n";
-questionList += "你可以說:\n";
-
-sameRes = {};
-sameRes["嗨"] =					{ body: "哈囉~" };
-sameRes["布丁狗"] =				{ body: "嗨嗨~我是布丁狗" };
-sameRes["你是誰"] =				{ body: "我是 Larry 跟 Annie 一起養的布丁狗" };
-sameRes["YA"] =					{ body: "YA個屁" };
-sameRes["今天要吃什麼呢"] =		{ body: "可以吃布丁阿~布丁好吃~" };
-sameRes["你有在打LOL嗎"] =		{ body: "有喔 我的暱稱叫統二布丁" };
-sameRes["你有女朋友嗎"] =		{ body: "我沒有女朋友\n但是我的主人有女朋友\n她叫王詩涵" };
+questionList += "你可以講像是這樣的話:\n";
+addQuestion("嗨嗨你好");
+addQuestion("哈囉布丁狗");
+addQuestion("你是誰阿");
+addQuestion("YA我今天撿到一百塊");
+addQuestion("今天晚餐要吃什麼呢");
+addQuestion("你有在打LOL嗎");
+addQuestion("你有交女朋友嗎");
+addQuestion("承億已經去睡了嗎");
 
 sysRes = {};
-sysRes["/stop"] = { body: "拜拜~" };
-sysRes["/sleep"] = { body: "Larry晚安~" };
-sysRes["/wake"] = { body: "早安Larry~" };
+sysRes["/stop"] =		{ body: "拜拜~" };
+sysRes["/sleep"] =		{ body: "Larry晚安~" };
+sysRes["/wake"] =		{ body: "早安Larry~" };
 
-amountOfQuestion = 0;
 larryIsSleep = false;
 
-for(req in sameRes){
-	addQuestion(req);
-}
-addQuestion("承億睡了嗎");
-
 console.log("init respond OK");
-
-
-
 
 
 
@@ -38,52 +29,72 @@ function addQuestion(question){
 	questionList += (amountOfQuestion.toString() + "." + question + "\n");
 }
 
-
-function multiRes(req){
-	switch(req){
-		case "承億睡了嗎":
-			if(larryIsSleep) return "他已經去睡了~\n你也早點睡吧";
-			return "還沒~";
-	}
-	return "";
-}
-
 function sysTask(req){
 	if(req === "/sleep") larryIsSleep = true;
 	else if(req === "/wake") larryIsSleep = false;
 }
 
-
-
-
-
-
-var getQuestion = function(){
-	return questionList;
-};
-
-
-var getResponse = function(req){
-	var str = sameRes[req];
-	if(str)	return str;
-	str = multiRes(req);
-	if(str)	return str;
-	return "";
-};
-
-var getSysResponse = function(req){
+function getSysResponse(req){
 	var str = sysRes[req];
 	if(str){
-		sysTask(req);
 		return str;
 	}
 	return "";
 };
 
+function getRegularResponse(req){
+	if(req.indexOf("承億") != -1 && req.indexOf("睡") != -1){
+		if(larryIsSleep)
+			return "他已經去睡了~\n你也早點睡吧";
+		return "還沒~";
+	}
 
-module.exports.getQuestion = getQuestion;
+	if(req.indexOf("YA") != -1){
+		return "YA個屁";
+	}
+
+	if(req.indexOf("吃") != -1){
+		return "吃布丁~布丁好吃~";
+	}
+
+	if(req.indexOf("lol") != -1){
+		return "我也有在打LOL喔 我的暱稱叫統二布丁";
+	}
+
+	if(req.indexOf("嗨") != -1 || req.indexOf("哈囉") != -1 || req.indexOf("hi") != -1 || req.indexOf("你好") != -1){
+		return "哈囉~";
+	}
+
+	if(req.indexOf("布丁狗") != -1){
+		return "嗨嗨~我是布丁狗";
+	}
+
+	if(req.indexOf("你是誰") != -1){
+		return "我是 Larry 跟 Annie 一起養的布丁狗";
+	}
+
+	if(req.indexOf("你有女朋友嗎") != -1){
+		return "我沒有女朋友\n但是我的主人有女朋友\n她叫王詩涵";
+	}
+
+	return questionList;
+}
+
+
+
+
+
+var getResponse = function(req){
+	if(req.charAt(0) == '/'){
+		sysTask(req);
+		return getSysResponse(req);
+	} else {
+		return getRegularResponse(req);
+	}
+};
+
+
 module.exports.getResponse = getResponse;
-module.exports.getSysResponse = getSysResponse;
 
 
 
